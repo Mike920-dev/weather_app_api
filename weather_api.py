228d871e4dbd753d46b6d292de863cd4e8e4db14
Mike-920-dev <65869511+Mike920-dev@users.noreply.github.com>
2021-02-10 15:43:29 +0100

    Make functions

--- weather_api.py
+++ weather_api.py
@@ -6,6 +6,20 @@ from pprint import pprint
 #declare a const that will be used in converting Kelvin degs into Celsius degs
 ZERO_DEGREES_KELVIN = 273.15
 
+#functions
+def get_json_response(api_url, headers_params, parameters):
+    response = requests.get(api_url, headers=headers_params, params=parameters)
+
+    return response
+
+def get_data_from_server(response):
+    try:
+        content = response.json()
+    except json.decoder.JSONDecodeError:
+        return "Nieprawidłowy format!"
+    else:
+        return content
+
 #Ask user about city and country
 city_country = input("Podaj miasto i kraj(np. Warszawa, pl): ")
 
@@ -15,34 +29,28 @@ params = {
     "lang": "pl"
 }
 
-#make a request
-response = requests.get('https://community-open-weather-map.p.rapidapi.com/weather', headers=credentials.headers, params=params)
+#make a request  https://community-open-weather-map.p.rapidapi.com/weather credentials.headers params
+response = get_json_response('https://community-open-weather-map.p.rapidapi.com/weather', credentials.headers, params)
 
 #here download data from rapidapis's server
-try:
-    content = response.json()
-except json.decoder.JSONDecodeError as error:
-    print(error)
-    print("Nieprawidłowy format!")
-else:
-    # pprint(content)
+content = get_data_from_server(response)
 
-    #Download info about temperature
-    currentTemperatureInfo = content['main']
+#Download info about temperature
+currentTemperatureInfo = content['main']
 
-    #default: Kelvin degrees, download info about current temp and feels_like temp
-    currentTemperatureKelvin = currentTemperatureInfo['temp']
-    feelsLikeTemperatureKelvin = currentTemperatureInfo['feels_like']
+#default: Kelvin degrees, download info about current temp and feels_like temp
+currentTemperatureKelvin = currentTemperatureInfo['temp']
+feelsLikeTemperatureKelvin = currentTemperatureInfo['feels_like']
 
-    #tests
-    # print(currentTemperatureKelvin)
-    # print(feelsLikeTemperatureKelvin)
+#tests
+# print(currentTemperatureKelvin)
+# # print(feelsLikeTemperatureKelvin)
 
-    #convert Kelvin degs into Celsius degs
-    currentTemperatureCelsius = round((currentTemperatureKelvin - ZERO_DEGREES_KELVIN), 2)
-    feelsLikeTempreatureCelsius = round((feelsLikeTemperatureKelvin - ZERO_DEGREES_KELVIN), 2)
+#convert Kelvin degs into Celsius degs
+currentTemperatureCelsius = round((currentTemperatureKelvin - ZERO_DEGREES_KELVIN), 2)
+feelsLikeTempreatureCelsius = round((feelsLikeTemperatureKelvin - ZERO_DEGREES_KELVIN), 2)
 
-    #display current temp and feels_like temp
-    print("Temperatura: " + str(currentTemperatureCelsius))
-    print("Temperatura odczuwalna: " + str(feelsLikeTempreatureCelsius))
+#display current temp and feels_like temp
+print("Temperatura: " + str(currentTemperatureCelsius))
+print("Temperatura odczuwalna: " + str(feelsLikeTempreatureCelsius))
     
